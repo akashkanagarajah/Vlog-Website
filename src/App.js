b030b04cc0fc3516dadf9731b690989fdc9d5a00
akashkanagarajah <akashkanagarajah@gmail.com>
2024-06-09 01:05:51 -0400

    Added Videos,photos, and about me page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from'./components/pages/Home';
+import AboutMe from './components/pages/AboutMe';
+import Photos from './components/pages/Photos';
+import Videos from './components/pages/Videos';
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact element={<Home/>} />
+          <Route path='/about-me' element={<AboutMe />} />
+          <Route path='/photos' element={<Photos />} />
+          <Route path='/videos' element={<Videos />} />
         </Routes>
       </Router>
     </>
